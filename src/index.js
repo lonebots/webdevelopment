71251686452a41099e432e9b115b0b3a503e1df7
jishnu <jishnusurajila@gmail.com>
2020-03-07 17:18:58 +0530

    new changes made in my website

--- src/index.js
+++ src/index.js
@@ -2,15 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import HomePage from "./HomePage";
+import MyInfo from "./components/MyInfo";
 
-ReactDOM.render(
-  <div>
-    <h1>My list!</h1>
-    <ul>
-      <li>hi</li>
-      <li>I'am</li>
-      <li>jishnu</li>
-    </ul>
-  </div>,
-  document.getElementById("root")
-);
+ReactDOM.render(<MyInfo />, document.getElementById("root"));
