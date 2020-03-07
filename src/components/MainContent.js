848b726594187c586c0a36b7ea9d6702bec73e09
jishnu <jishnusurajila@gmail.com>
2020-03-07 18:44:16 +0530

    important commit

---
+++ src/components/MainContent.js
@@ -0,0 +1,10 @@
+import React from "react";
+function MainContent() {
+  return (
+    <div>
+      <input type="Checkbox"></input>
+      <maincontent>this includes main content</maincontent>
+    </div>
+  );
+}
+export default MainContent;
---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <input type="Checkbox"></input>
+      <navbar>this include Navigation BAR</navbar>
+    </div>
+  );
+}
+export default Navbar;
