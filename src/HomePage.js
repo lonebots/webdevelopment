d4ba444cd1af4462067e77636cac05f7c17f93e5
jishnu <jishnusurajila@gmail.com>
2020-03-07 15:07:10 +0530

    new source file for the web application

---
+++ src/HomePage.js
@@ -0,0 +1,9 @@
+import { React, Component } from "react";
+
+class HomePage extends Component {
+  render() {
+    return <h1>"hi11111"</h1>;
+  }
+}
+
+export default HomePage;
---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import HomePage from "./HomePage";
+
+ReactDOM.render(
+  <div>
+    <h1>My list!</h1>
+    <ul>
+      <li>hi</li>
+      <li>I'am</li>
+      <li>jishnu</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
