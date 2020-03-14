ebb219e70cb550163a93fc36cecac93d1810b8d1
jishnu <jishnusurajila@gmail.com>
2020-03-14 15:51:47 +0530

    new commit

--- src/App.js
+++ src/App.js
@@ -2,8 +2,10 @@ import React from "react";
 //import Joke from "./joke";
 import todoData from "./components/tododata";
 import Listings from "./components/Listing";
+import "./components/assets/css/list.css";
 export default App => {
-  const todoItem = todoData.map(item => <Listings key={item.id} item={item} />);
-
-  return <div className="content-view">{todoItem}</div>;
+  const todocomponents = todoData.map(listitem => (
+    <Listings key={listitem.id} listitem={listitem} />
+  ));
+  return <div className="content-view">{todocomponents}</div>;
 };
--- src/components/Listing.js
+++ src/components/Listing.js
@@ -1,12 +1,11 @@
 import React from "react";
 import "./assets/css/list.css";
-
 export default function Listing(props) {
   return (
     <div className="ckbox">
       <p>
-        <input type="Checkbox"></input>
-        {props.item.text}
+        <input type="Checkbox" checked={props.listitem.completed} />
+        {props.listitem.text}
       </p>
     </div>
   );
